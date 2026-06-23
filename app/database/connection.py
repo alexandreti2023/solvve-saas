f2a2562ff75be33e70546d09322436697f9f0515
ALEXANDRE <ALEXANDRE@DARCI.LOCAL>
2026-06-23 14:49:01 -0300

    Migra para PostgreSQL

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./solvve.db"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./solvve.db"
 )
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
